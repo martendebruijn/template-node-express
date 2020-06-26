f1a2839bdba31e1502d71d73c4dc5a3d6216443c
Marten de Bruijn <martenbruijn@gmail.com>
2020-06-26 11:38:05 +0200

    made folders and files

---
+++ server/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const express = require('express'),
+  router = require('./modules/router.js'),
+  path = require('path'),
+  compression = require('compression');
+
+const port = process.env.PORT || 3000;
+
+app
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.urlencoded({ extended: true }))
+  .use(compression())
+  .set('views', __dirname + '/views/')
+  .set('view engine', 'ejs')
+  .get('/', router.home);
+
+const server = app.listen(port, () =>
+  console.log(`App listening on port ${port}`)
+);
