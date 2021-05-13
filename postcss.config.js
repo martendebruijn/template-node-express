841289c16c4b1ef9892fbfaa7f68c8c6ac7d7c51
Marten de Bruijn <hallo@martendebruijn.nl>
2021-05-13 23:05:33 +0200

    replaced gulp with npm scripts

---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+module.exports = (ctx) => ({
+  plugins: {
+    "postcss-sorting": {
+      order: [
+        "custom-properties",
+        "dollar-variables",
+        "declarations",
+        "at-rules",
+        "rules",
+      ],
+
+      "properties-order": "alphabetical",
+
+      "unspecified-properties-position": "bottom",
+    },
+    autoprefixer: {},
+    cssnano: { preset: "default" },
+  },
+});
