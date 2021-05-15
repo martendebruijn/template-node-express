f73dc5d09064f5fe3bbb601a80fdf38641eb939b
Marten de Bruijn <hallo@martendebruijn.nl>
2021-05-15 19:13:59 +0200

    removed test files and added right file structure

--- server/src/js/main.js
+++
@@ -1,9 +0,0 @@
-// Enter your client JavaScript
-console.log("im from main");
-console.log("im from main");
-console.log("im from main");
-console.log("im from main");
-console.log("im from main");
-console.log("im from main");
-console.log("im from main");
-console.log("im from main");
